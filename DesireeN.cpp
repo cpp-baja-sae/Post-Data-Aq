6b9285cc7b2d91a00d9ddfd2978ff4d2134519fa
Forrest <fcglover@cpp.edu>
2021-09-12 21:53:58 -0700

    Base setup

---
+++ DesireeN.cpp
@@ -0,0 +1,14 @@
+#include "DesireeN.h"
+void wut_up() {
+  // do stuff;
+}
+
+int main() {
+
+  wut_up();
+  return 0;
+}
+
+void wut_up() {
+  // do stuff;
+}
---
+++ DesireeN.h
@@ -0,0 +1,8 @@
+#ifndef __DESRIEEN_H__
+#define __DESIREEN_H__
+#define blah 1
+
+int heyo = 12;
+
+void wut_up();
+#endif
