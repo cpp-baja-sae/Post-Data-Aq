6e25065128382b930f2c30a79c6abc9e5c784276
Forrest <fcglover@cpp.edu>
2021-10-22 20:45:37 -0700

    Files were reformated

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,24 @@
 #include "main.h"
 #include "error_checking.h"
-#include <iostream>
 #include <chrono>
+#include <iostream>
+
 
 int main(int argc, char **argv) {
-    std::string var_dummy={"this is my WORLD >:)"};
-    auto start = std::chrono::high_resolution_clock::now();
-hammingcode(var_dummy);
-auto stop = std::chrono::high_resolution_clock::now();
-auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
- std::cout << "Time taken by function: "
-         << duration.count() << " microseconds" << std::endl;
-    /*
-    for (int five = 5; five >= 0; five--) {
-        for (int i = 0; i < argc; i++) {
-            std::cout << argv[i] << "\n";
-        }
-    }
-    */
-    return 0; 
+  std::string var_dummy = {"this is my WORLD >:)"};
+  auto start = std::chrono::high_resolution_clock::now();
+  hammingcode(var_dummy);
+  auto stop = std::chrono::high_resolution_clock::now();
+  auto duration =
+      std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+  std::cout << "Time taken by function: " << duration.count() << " microseconds"
+            << std::endl;
+  /*
+  for (int five = 5; five >= 0; five--) {
+      for (int i = 0; i < argc; i++) {
+          std::cout << argv[i] << "\n";
+      }
+  }
+  */
+  return 0;
 }
