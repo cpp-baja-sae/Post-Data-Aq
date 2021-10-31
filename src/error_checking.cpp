2fde88d2cb84bcc405902e05cecbb19f598299e3
Janelle <janelle4044@gmail.com>
2021-10-30 20:39:57 -0700

    more changes in variables

--- src/error_checking.cpp
+++ src/error_checking.cpp
@@ -8,22 +8,23 @@ int hammingcode(string input) {
   int data_bits[128000];
   int data_size;
   int redundant_bits = 0;
-  int parity; // m = no. of data bits, r = no. of redundant bits
+  int parity; 
 
   cout<<"Enter the size of data bits: ";
   cin>>data_size;
-  data_size = 128000;
+  
 
   // finding no. of redundant bits
-  // hamming code equation to find redundant bits??? 
-  while (pow(2, redundant_bits) < data_size + redundant_bits + 1) {
+ #define SQUARE_LAW 2
+  while (pow(SQUARE_LAW, redundant_bits) < data_size + redundant_bits + 1) {
     redundant_bits++;
   }
 
    cout<<"Enter the data bit: ";
   for (int i = 1; i <= data_size; i++){
      cin>>data_bits[i];
-    data_bits[i] = 1;}
+    // data_bits[i] = 1;
+    }
 
   int hamming[data_size + redundant_bits];
   int redun_exponent = 0;
@@ -32,8 +33,8 @@ int hammingcode(string input) {
   // finding positions of redundant bits.
   for (int i = 1; i <= data_size + redundant_bits; i++) {
 
-    if (i == pow(2, redun_exponent)) {
-      hamming[i] = 0;
+    if (i == pow(SQUARE_LAW, redun_exponent)) {
+      hamming[i] = -1;
       redun_exponent++;
     } else {
       hamming[i] = data_bits[index_input];
@@ -45,26 +46,25 @@ int hammingcode(string input) {
   int x = 0;
   int min = 0;
   int max = 0;
-  int j ;
-  //int parity;
+  int j = 0;
+  //int parity = 0
   // finding parity bit
-  for (int i = 1; i <= data_size + redundant_bits; i = pow(2, redundant_bit_position)) {
+  for (int i = 1; i <= data_size + redundant_bits; i = pow(SQUARE_LAW, redundant_bit_position)) {
     redundant_bit_position++;
     parity = 0;
     j = i;
     x = i;
-    min = 1;
-    max = i;
+    int xor_min = 1;
+    int xor_max = i;
     while (j <= data_size + redundant_bits) {
-      for (x = i; max >= min && x <= data_size + redundant_bits; min++, x++) {
+      for (x = j; xor_max >= xor_min && x <= data_size + redundant_bits; xor_min++, x++) {
         if (hamming[x] == 1)
           parity = parity + 1;
-        ;
-         cout << x << "\n";
+         //cout << x << "\n";
       }
-      // cout << x << "\n";
+      //cout<<"\n" << j << "\n";
       j = x + i;
-      min = 1;
+      xor_min = 1;
     }
 
     // checking for even parity
@@ -78,7 +78,7 @@ int hammingcode(string input) {
   cout<<"\nHamming code is: ";
   for(int i = 1; i <= data_size + redundant_bits ; i++){
    cout<<hamming[i]<<" ";
-   cout <<"\n"<< index_input<< "\n";}
+   }
 
-  return 8383;
+  return 0;
 }
