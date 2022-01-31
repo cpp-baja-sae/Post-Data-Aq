abfaf806436b3343d147d7bf1615ef6dc985838b
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2022-01-30 16:03:06 -0800

    Is this any faster?

--- rust_data_server/src/bin/unpack.rs
+++ rust_data_server/src/bin/unpack.rs
@@ -19,6 +19,7 @@ fn main() {
     let name = &args[1];
     let descriptor = rust_data_server::example_file_descriptor();
     let input = File::open(format!("data/{}.bin", name)).unwrap();
+    let total_bytes = input.metadata().unwrap().len();
     let mut input = BufReader::new(input);
     let output_dir = format!("cache/{}", name);
     fs::create_dir_all(&output_dir).unwrap();
@@ -39,6 +40,7 @@ fn main() {
             }
         }
     }
+    let mut outputs: Vec<_> = outputs.into_iter().collect();
 
     let mut frame_sequence_size = 0;
     for frame in descriptor.frame_sequence() {
@@ -47,31 +49,50 @@ fn main() {
         }
     }
 
+    let mut read_sequence = Vec::new();
+    for frame in descriptor.frame_sequence() {
+        for typ in frame {
+            read_sequence.push((
+                typ.num_packed_bytes(),
+                typ,
+                typ.unpacked_types()
+                    .into_iter()
+                    .map(|unpacked_typ| {
+                        outputs
+                            .iter()
+                            .position(|(out_type, _file)| out_type == &unpacked_typ)
+                            .unwrap()
+                    })
+                    .collect::<Vec<_>>(),
+            ));
+        }
+    }
+
     let mut data_frame = vec![0u8; frame_sequence_size];
+    let mut bytes_so_far = 0;
+    let mut last_notification = 0;
     while let Ok(()) = input.read_exact(&mut data_frame) {
         if let Err(_) = hamming::decode(&mut data_frame) {
             panic!("Data is corrupt.");
         }
         let mut frame_ptr = &data_frame[..];
-        let mut progress = 0;
-        for frame in descriptor.frame_sequence() {
-            for typ in frame {
-                progress += 1;
-                println!("{}", progress);
-                let num_bytes = typ.num_packed_bytes();
-                let out_types = typ.unpacked_types();
-                let values = typ
-                    .unpack(&frame_ptr[..num_bytes])
-                    .expect("data is corrupt");
-                for (out_typ, value) in out_types.into_iter().zip(values.into_iter()) {
-                    outputs
-                        .get_mut(&out_typ)
-                        .unwrap()
-                        .write_all(&value.to_le_bytes())
-                        .unwrap();
-                }
-                frame_ptr = &frame_ptr[num_bytes..];
+        for (length, unpacker, destinations) in &read_sequence {
+            let length = *length;
+            let values = unpacker
+                .unpack(&frame_ptr[..length])
+                .expect("Data is corrupt");
+            for (&destination, value) in destinations.iter().zip(values.into_iter()) {
+                outputs[destination]
+                    .1
+                    .write_all(&value.to_le_bytes())
+                    .unwrap();
             }
+            frame_ptr = &frame_ptr[length..];
+        }
+        bytes_so_far += frame_sequence_size;
+        if bytes_so_far - last_notification > 10_000_000 {
+            println!("{}M/{}M", bytes_so_far / 1_000_000, total_bytes / 1_000_000);
+            last_notification = bytes_so_far;
         }
     }
 }
