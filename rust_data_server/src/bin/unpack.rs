5f8dbcfe4a30c78083887dac50baf46233772290
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2022-01-30 17:09:55 -0800

    Tweaks

--- rust_data_server/src/bin/unpack.rs
+++ rust_data_server/src/bin/unpack.rs
@@ -3,9 +3,10 @@ use std::env;
 fn main() {
     let name = get_name_from_command_line();
     let descriptor = rust_data_server::example_file_descriptor();
-    rust_data_server::ingestion::unpack(&name, descriptor, |bytes_so_far, total_bytes| {
+    let progress_printer = |bytes_so_far, total_bytes| {
         println!("{}M/{}M", bytes_so_far / 1_000_000, total_bytes / 1_000_000)
-    })
+    };
+    rust_data_server::ingestion::unpack(&name, descriptor, progress_printer)
 }
 
 fn get_name_from_command_line() -> String {
--- rust_data_server/src/ingestion/unpack/execution.rs
+++ rust_data_server/src/ingestion/unpack/execution.rs
@@ -13,21 +13,28 @@ pub fn unpack(
 ) {
     let (mut progress_tracker, mut input) = setup::load_input_file(&name);
     let output_dir = setup::create_output_dir(&name);
-
-    let (mut outputs, sample_rate_multipliers) =
-        setup::get_output_info(&descriptor, output_dir);
+    let (mut outputs, sample_rate_multipliers) = setup::get_output_info(&descriptor, output_dir);
+    // How long a single instance of the descriptor's frame sequence is, dictates
+    // how much data we try to read at once.
     let frame_sequence_size = descriptor.frame_sequence_size();
 
+    // This defines a series of operations to perform repeatedly to read from the
+    // file. It specifies how many bytes to read at a time, what to use to decode
+    // those bytes, and where the decoded floats should go.
     let read_sequence = setup::build_read_sequence(&descriptor, &outputs);
 
+    // read_sequence says how to read a single occurence of the entire frame
+    // sequence specified in the descriptor.
     let mut data_frame = vec![0u8; frame_sequence_size];
     while let Ok(()) = input.read_exact(&mut data_frame) {
         if let Err(_) = hamming::decode(&mut data_frame) {
             panic!("Data is corrupt.");
         }
+        // This is the variable decoders read data from.
         let mut frame_ptr = &data_frame[..];
         for step in &read_sequence {
             execute_read_step(step, frame_ptr, &mut outputs);
+            // Advance the pointer for the next decoder.
             frame_ptr = &frame_ptr[step.num_bytes..];
         }
         progress_tracker.advance(frame_sequence_size as u64, &mut progress_callback);
@@ -35,10 +42,12 @@ pub fn unpack(
 }
 
 fn execute_read_step(step: &ReadStep, frame_ptr: &[u8], outputs: &mut Outputs) {
+    // Unpack data into individual values.
     let values = step
         .unpacker
         .unpack(&frame_ptr[..step.num_bytes])
         .expect("Data is corrupt");
+    // Use the destinations in the read step to write the unpacked data to files.
     for (&destination, value) in step.destinations.iter().zip(values.into_iter()) {
         outputs[destination]
             .1
